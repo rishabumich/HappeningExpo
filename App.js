e932e6cff5b29c238752c12f13da35f767eac3c7
marschub <marschub@umich.edu>
2023-12-02 15:32:50 -0500

    map with marker, centered at ann arbor

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    "latitude": 42.27726715190734, 
+    "latitudeDelta": 0.03415826961671797, 
+    "longitude": -83.73963831191361, 
+    "longitudeDelta": 0.023597393932092814
+  });
+  const userLocation = async() => {
+    let { status } = await Location.requestForegroundPermissionsAysnc();
+    
+  }
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}
+        region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='Marker' />
+      </MapView>
     </View>
   );
 }
