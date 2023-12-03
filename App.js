a9617a9838a89fd08953e5dbd8c0acce989952ca
mschubs <alexsmarcus1980@gmail.com>
2023-12-03 15:28:59 -0500

    cleanup commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import Map from './components/Map';
-import { LatLng } from 'react-native-maps';
 
 export default function App() {
 
   const points = [
     {coordinate: {latitude: 42.27726715190734, longitude: -83.73963831191361}, label: "diag"},
-    {coordinate: {latitude: 42.27704467373761, longitude: -83.74435684207712}, label: "home"}
+    {coordinate: {latitude: 42.27704467373761, longitude: -83.74435684207712}, label: "home"},
+    {coordinate: {latitude: 42.29208392690929, longitude: -83.71578585061047}, label: "the grove"}
   ];
   
   return (
